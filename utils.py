8f2c1315726b9657a42575cb21b51fccccd9f8b3
200554E <129167986+200554E@users.noreply.github.com>
2025-03-22 01:55:50 +0530

    Updated utils.py for INVERTER

--- utils.py
+++ utils.py
@@ -90,7 +90,7 @@ def evaluate_score(y_real, y_predict, y_real_c, y_pred_c, logger):
     return maeScore
 
 def evaluate_score_multi(y_real, y_predict, y_real_c, y_pred_c, logger):
-    listOfAppliance = ['washing machine', 'dishwasher', 'fridge', 'microwave']
+    listOfAppliance = ['dish_washer', 'fridge', 'microwave', 'washer_dryer', 'inverter_ac']
     mapeScore = []
     for i in range(y_predict.shape[1]):
         logger.info(f"Evaluate {listOfAppliance[i]}: ")
