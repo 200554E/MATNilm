610be8103dee0b17ae4f586cfea8a1c6f02fffac
200554E <129167986+200554E@users.noreply.github.com>
2025-03-30 14:46:28 +0530

    shape 5 (but set thresholds later)

--- utils.py
+++ utils.py
@@ -90,7 +90,7 @@ def evaluate_score(y_real, y_predict, y_real_c, y_pred_c, logger):
     return maeScore
 
 def evaluate_score_multi(y_real, y_predict, y_real_c, y_pred_c, logger):
-    listOfAppliance = ["main", "dish washer", "fridge", "microwave", "washer dryer", "iron"]
+    listOfAppliance = ['dish washer', 'fridge', 'microwave', 'washer dryer', 'iron']
     mapeScore = []
     for i in range(y_predict.shape[1]):
         logger.info(f"Evaluate {listOfAppliance[i]}: ")
