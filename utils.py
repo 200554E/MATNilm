3d16e3f6bd7656a920999638f199ddf9a6a696b0
200554E <129167986+200554E@users.noreply.github.com>
2025-04-13 00:32:56 +0530

    water pump

--- utils.py
+++ utils.py
@@ -90,7 +90,7 @@ def evaluate_score(y_real, y_predict, y_real_c, y_pred_c, logger):
     return maeScore
 
 def evaluate_score_multi(y_real, y_predict, y_real_c, y_pred_c, logger):
-    listOfAppliance = ['microwave', 'washer_dryer', 'dishwasher', '3ph_fridge_1']
+    listOfAppliance = ['microwave', 'washer_dryer', 'dishwasher', 'water_pump']
     mapeScore = []
     for i in range(y_predict.shape[1]):
         logger.info(f"Evaluate {listOfAppliance[i]}: ")
